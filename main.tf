a54f87f14cdacf72fb2c0e8fab1104e2c81f5110
Dinesh-1507-developer <dineshpragatheesh@gmail.com>
2026-03-19 13:13:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
-  instance_state = "stopped" 
 
   tags = {
     Name = "GitHub-Terraform-EC2"
